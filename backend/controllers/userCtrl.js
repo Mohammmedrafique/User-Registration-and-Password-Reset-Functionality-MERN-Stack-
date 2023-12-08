f50c4049f5418fb98ff71158a3399530e9b756c9
Mohammmedrafique <pushpendrasharmakarauli@gmail.com>
2023-12-09 00:25:24 +0530

    d

--- backend/controllers/userCtrl.js
+++ backend/controllers/userCtrl.js
@@ -1,121 +1,9 @@
-// const userModel = require("../models/userModels");
-// const bcrypt = require("bcryptjs");
-// const jwt = require("jsonwebtoken");
-
-// // Register callback
-// const registerController = async (req, res) => {
-//   try {
-//     const existingUser = await userModel.findOne({ email: req.body.email });
-//     if (existingUser) {
-//       return res
-//         .status(200)
-//         .send({ message: "User Already Exists", success: false });
-//     }
-//     const password = req.body.password;
-//     const salt = await bcrypt.genSalt(10);
-//     const hashedPassword = await bcrypt.hash(password, salt);
-//     req.body.password = hashedPassword;
-//     const newUser = new userModel(req.body);
-//     await newUser.save();
-//     res.status(201).send({ message: "Registered Successfully", success: true });
-//   } catch (error) {
-//     console.log(error);
-//     res.status(500).send({
-//       success: false,
-//       message: `Register Controller ${error.message}`,
-//     });
-//   }
-// };
-
-// // Login callback
-// const loginController = async (req, res) => {
-//   try {
-//     const user = await userModel.findOne({ email: req.body.email });
-//     if (!user) {
-//       return res
-//         .status(200)
-//         .send({ message: "User not found", success: false });
-//     }
-//     const isMatch = await bcrypt.compare(req.body.password, user.password);
-//     if (!isMatch) {
-//       return res
-//         .status(200)
-//         .send({ message: "Invalid Email or Password", success: false });
-//     }
-//     const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
-//       expiresIn: "1d",
-//     });
-//     res.status(200).send({ message: "Login Success", success: true, token });
-//   } catch (error) {
-//     console.log(error);
-//     res.status(500).send({ message: `Error in Login CTRL ${error.message}` });
-//   }
-// };
-
-// const forgotPasswordController = async (req, res) => {
-//   const { email } = req.body;
-//   UserModel.findOne({ email: email }).then((user) => {
-//     if (!user) {
-//       return res.send({ Status: "User not existed" });
-//     }
-//     const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
-//       expiresIn: "1d",
-//     });
-//     var transporter = nodemailer.createTransport({
-//       service: "gmail",
-//       auth: {
-//         user: "seowork11100@gmail.com",
-//         pass: "xmdu oebi hpeo drwg",
-//       },
-//     });
-
-//     var mailOptions = {
-//       from: "seowork11100@gmail.com",
-//       to: "user email@gmail.com",
-//       subject: "Reset Password Link",
-//       text: `http://localhost:5173/reset_password/${user._id}/${token}`,
-//     };
-
-//     transporter.sendMail(mailOptions, function (error, info) {
-//       if (error) {
-//         console.log(error);
-//       } else {
-//         return res.send({ Status: "Success" });
-//       }
-//     });
-//   });
-// };
-
-// const resetPasswordController = async (req, res) => {
-//   const { id, token } = req.params;
-//   const { password } = req.body;
-
-//   jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
-//     if (err) {
-//       return res.json({ Status: "Error with token" });
-//     } else {
-//       bcrypt
-//         .hash(password, 10)
-//         .then((hash) => {
-//           UserModel.findByIdAndUpdate({ _id: id }, { password: hash })
-//             .then((u) => res.send({ Status: "Success" }))
-//             .catch((err) => res.send({ Status: err }));
-//         })
-//         .catch((err) => res.send({ Status: err }));
-//     }
-//   });
-// };
-// module.exports = {
-//   loginController,
-//   registerController,
-//   resetPasswordController,
-//   forgotPasswordController,
-// };
 const userModel = require("../models/userModels");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
-const nodemailer = require("nodemailer"); // Assuming nodemailer is required
-
+const nodemailer = require("nodemailer");
+const dotenv = require("dotenv");
+dotenv.config();
 const registerController = async (req, res) => {
   try {
     const existingUser = await userModel.findOne({ email: req.body.email });
@@ -136,12 +24,10 @@ const registerController = async (req, res) => {
     res.status(201).send({ message: "Registered Successfully", success: true });
   } catch (error) {
     console.log(error);
-    res
-      .status(500)
-      .send({
-        success: false,
-        message: `Register Controller ${error.message}`,
-      });
+    res.status(500).send({
+      success: false,
+      message: `Register Controller ${error.message}`,
+    });
   }
 };
 
@@ -189,14 +75,14 @@ const forgotPasswordController = async (req, res) => {
     var transporter = nodemailer.createTransport({
       service: "gmail",
       auth: {
-        user: "seowork11100@gmail.com",
-        pass: "xmdu oebi hpeo drwg",
+        user: process.env.EMAIL,
+        pass: process.env.EPASS,
       },
     });
 
     var mailOptions = {
-      from: "seowork11100@gmail.com",
-      to: user.email, // Use the user's email from the database
+      from: process.env.EMAIL,
+      to: user.email,
       subject: "Reset Password Link",
       text: resetLink,
     };
--- frontend/src/pages/ForgotPassword.jsx
+++ frontend/src/pages/ForgotPassword.jsx
@@ -1,3 +1,72 @@
+// import { useState } from "react";
+// import { Link, useNavigate } from "react-router-dom";
+// import axios from "axios";
+
+// function ForgotPassword() {
+//   const [email, setEmail] = useState("");
+//   const navigate = useNavigate();
+
+//   axios.defaults.withCredentials = true;
+
+//   const handleSubmit = (e) => {
+//     e.preventDefault();
+
+//     axios
+//       .post("http://localhost:8000/api/user/forgot-password", { email })
+//       .then((res) => {
+//         if (res.data.Status === "Success") {
+//           alert("Password reset link sent to your email");
+//           navigate("/login");
+//         } else {
+//           console.log("Password reset request failed:", res.data.Status);
+//         }
+//       })
+//       .catch((err) => {
+//         console.error("Error sending password reset request:", err);
+//       });
+//   };
+
+//   return (
+//     <div className="flex justify-center items-center bg-gray-800 min-h-screen">
+//       <div className="bg-white p-8 rounded shadow-lg w-1/3">
+//         <h4 className="text-2xl font-bold mb-4">Forgot Password</h4>
+//         <form onSubmit={handleSubmit}>
+//           <div className="mb-4">
+//             <label
+//               htmlFor="email"
+//               className="block text-sm font-medium text-gray-600"
+//             >
+//               Email
+//             </label>
+//             <input
+//               type="email"
+//               placeholder="Enter Email"
+//               autoComplete="off"
+//               name="email"
+//               className="mt-1 p-2 w-full border rounded-md"
+//               value={email}
+//               onChange={(e) => setEmail(e.target.value)}
+//             />
+//           </div>
+//           <button
+//             type="submit"
+//             className="bg-green-500 text-white p-2 w-full rounded-md hover:bg-green-600"
+//           >
+//             Send
+//           </button>
+//         </form>
+//         <p className="mt-4 text-gray-600">
+//           Remember your password?{" "}
+//           <Link to="/login" className="text-blue-500">
+//             Login
+//           </Link>
+//         </p>
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default ForgotPassword;
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import axios from "axios";
@@ -14,10 +83,13 @@ function ForgotPassword() {
     axios
       .post("http://localhost:8000/api/user/forgot-password", { email })
       .then((res) => {
-        if (res.data.Status === "Success") {
+        // console.log("Response:", res);
+        const status = res.data && res.data.status;
+        if (status === "Success") {
+          alert("Password reset link sent to your email");
           navigate("/login");
         } else {
-          console.log("Password reset request failed:", res.data.Status);
+          console.log("Password reset request failed:", status);
         }
       })
       .catch((err) => {
