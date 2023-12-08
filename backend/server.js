96fef2f2fd78e379c07de208035278a599a3d477
Mohammmedrafique <pushpendrasharmakarauli@gmail.com>
2023-12-09 00:59:00 +0530

    g

--- backend/server.js
+++ backend/server.js
@@ -18,7 +18,7 @@ app.use(express.json());
 app.use(morgan("dev"));
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://user-registration-and-password-reset-functionality-mern-stack.vercel.app",
     credentials: true,
   })
 );
