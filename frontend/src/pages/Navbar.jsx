849902ffdf1335c5b3ecf116359b27098e80fcc8
Mohammmedrafique <pushpendrasharmakarauli@gmail.com>
2023-12-09 14:54:53 +0530

    d

--- frontend/src/pages/Navbar.jsx
+++ frontend/src/pages/Navbar.jsx
@@ -9,19 +9,21 @@ const Navbar = () => {
   return (
     <nav className="bg-blue-500 p-4">
       <div className="container mx-auto flex justify-between items-center">
-        <Link to="/home" className="text-white font-bold text-lg">
+        <Link to="#" className="text-white font-bold text-lg">
           Robo
         </Link>
         <div className="space-x-4">
-          <Link to="/home" className="text-white">
-            Home
-          </Link>
+          {isLoggedIn && (
+            <Link to="/home" className="text-white">
+              Home
+            </Link>
+          )}
           {!isLoggedIn ? (
             <Link to="/login" className="text-white">
               Login
             </Link>
           ) : (
-            <Link to="/login" className="text-white" onClick={logout}>
+            <Link to="/logout" className="text-white" onClick={logout}>
               Logout
             </Link>
           )}
