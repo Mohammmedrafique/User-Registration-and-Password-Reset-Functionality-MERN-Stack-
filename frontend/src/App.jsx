abf7fce2923688ae51195e69ae678d4a5d03648a
Mohammmedrafique <pushpendrasharmakarauli@gmail.com>
2023-12-09 10:03:58 +0530

    D

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,11 @@
 import Mroutes from "./pages/Mroutes";
 import Navbar from "./pages/Navbar";
-import ResetPassword from "./pages/ResetPassword";
 
 const App = () => {
   return (
     <div>
       <Navbar />
       <Mroutes />
-      <ResetPassword/>
     </div>
   );
 };
